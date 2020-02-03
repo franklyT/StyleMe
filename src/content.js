dcdf73031467b9cde8787cace53e755b12e0c793
franklyt <franklyt@users.noreply.github.com>
2020-02-03 13:15:16 -0500

    tsdc

--- src/content.js
+++ src/content.js
@@ -6,22 +6,26 @@ function styleMeLoader() {
     Style_Me.id = 'StyleMe-Master-Node';
     document.getElementsByTagName('head')[0].appendChild(Style_Me);
 }
-function processNodes() {
-    document.body
-        .querySelectorAll('*:not(script)')
-        .forEach(function (elm) {
+function lightenAllText() {
+    document.body.querySelectorAll('*:not(script)').forEach(function (elm) {
         styleMe.addStyle(styleMe.generateKey(), "color: lime !important;");
         elm.classList.add("" + styleMe.getLastKey());
+    });
+    document.getElementById('StyleMe-Master-Node').innerHTML = styleMe.getStyles().join('');
+}
+function processNodes() {
+    document.body.querySelectorAll('*:not(script)').forEach(function (elm) {
         var computeStyles = window.getComputedStyle(elm);
         if (styleMe.help.isLight(computeStyles.getPropertyValue('background-color'))) {
             styleMe.addStyle(styleMe.generateKey(), "background: " + styleMe.help.lightenDarkenColor(computeStyles.getPropertyValue('background-color'), 90) + " !important;");
             elm.classList.add("" + styleMe.getLastKey());
-            document.getElementById('StyleMe-Master-Node').innerHTML = styleMe.getStyles().join('');
         }
     });
+    document.getElementById('StyleMe-Master-Node').innerHTML = styleMe.getStyles().join('');
 }
 // remove from load, add mutation observer
 window.addEventListener('load', function () {
     styleMeLoader();
+    lightenAllText();
     processNodes();
 });
--- src/content.ts
+++ src/content.ts
@@ -9,36 +9,40 @@ function styleMeLoader() {
   document.getElementsByTagName('head')[0].appendChild(Style_Me);
 }
 
+function lightenAllText() {
+  document.body.querySelectorAll('*:not(script)').forEach((elm: any) => {
+    styleMe.addStyle(styleMe.generateKey(), `color: lime !important;`);
+    elm.classList.add(`${styleMe.getLastKey()}`);
+  });
+  document.getElementById(
+    'StyleMe-Master-Node'
+  )!.innerHTML = styleMe.getStyles().join('');
+}
+
 function processNodes() {
-  document.body
-    .querySelectorAll('*:not(script)')
-    .forEach((elm: any) => {
+  document.body.querySelectorAll('*:not(script)').forEach((elm: any) => {
+    let computeStyles = window.getComputedStyle(elm);
+    if (
+      styleMe.help.isLight(computeStyles.getPropertyValue('background-color'))
+    ) {
       styleMe.addStyle(
         styleMe.generateKey(),
-        `color: lime !important;`)
-        elm.classList.add(`${styleMe.getLastKey()}`);
-
-      let computeStyles = window.getComputedStyle(elm);
-      if (
-        styleMe.help.isLight(computeStyles.getPropertyValue('background-color'))
-      ) {
-        styleMe.addStyle(
-          styleMe.generateKey(),
-          `background: ${styleMe.help.lightenDarkenColor(
-            computeStyles.getPropertyValue('background-color'),
-            90
-          )} !important;`
-        );
-        elm.classList.add(`${styleMe.getLastKey()}`);
-        document.getElementById(
-          'StyleMe-Master-Node'
-        )!.innerHTML = styleMe.getStyles().join('');
-      }
-    });
+        `background: ${styleMe.help.lightenDarkenColor(
+          computeStyles.getPropertyValue('background-color'),
+          90
+        )} !important;`
+      );
+      elm.classList.add(`${styleMe.getLastKey()}`);
+    }
+  });
+  document.getElementById(
+    'StyleMe-Master-Node'
+  )!.innerHTML = styleMe.getStyles().join('');
 }
 
 // remove from load, add mutation observer
 window.addEventListener('load', () => {
   styleMeLoader();
+  lightenAllText();
   processNodes();
 });
