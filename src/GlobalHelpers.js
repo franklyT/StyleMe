c3a2ced68fb7b1e561c485cebd9a2e82fabf8e69
franklyt <franklyt@users.noreply.github.com>
2020-02-03 02:17:34 -0500

    qa

--- src/GlobalHelpers.js
+++ src/GlobalHelpers.js
@@ -21,18 +21,11 @@ var Helpers = /** @class */ (function () {
         var r, g, b, hsp;
         // Check the format of the color, HEX or RGB?
         if (color.match(/^rgb/)) {
-            // If HEX --> store the red, green, blue values in separate variables
+            // If RGB --> Convert it to HEX: http://gist.github.com/93661
             color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
             r = color[1];
             g = color[2];
             b = color[3];
-        }
-        else {
-            // If RGB --> Convert it to HEX: http://gist.github.com/983661
-            color = +('0x' + color.slice(1).replace(color.length < 5 && /./g, '$&$&'));
-            r = color >> 16;
-            g = (color >> 8) & 255;
-            b = color & 255;
         }
         // HSP equation from http://alienryderflex.com/hsp.html
         hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
